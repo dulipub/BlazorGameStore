6702876027b7680736c8feee5f03b33f31a9fff3
dulipub <dulipub@gmail.com>
2024-10-12 23:16:06 +0530

    Added home page

---
+++ BlazorGameStore/Models/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorGameStore.Models;
+
+public class Game
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+}
